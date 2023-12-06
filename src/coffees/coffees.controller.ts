eae550358f3169033a558b33d0e71f5934b1e423
Junior Hirwa <iamhirwejr@gmail.com>
2023-12-06 16:00:51 +0200

    feat: add Param decorator for dynamic routes

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
   findAll() {
     return 'this Action returns all the coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `this Action returns a coffee located at id: ${id}`;
   }
 }
