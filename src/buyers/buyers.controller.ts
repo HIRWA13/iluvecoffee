bb90647ee8ba46566c8f00b45eb16fe4509d4056
Junior Hirwa <iamhirwejr@gmail.com>
2023-12-06 18:22:51 +0200

    feat: update the buyers controller

--- src/buyers/buyers.controller.ts
+++ src/buyers/buyers.controller.ts
@@ -1,9 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 
 @Controller('buyers')
 export class BuyersController {
   @Get()
   findBuyers() {
     return 'this returns buyers';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `this returns information of the buyer with id: ${id}`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  createBuyer(@Body() buyer) {
+    return buyer;
   }
 }
