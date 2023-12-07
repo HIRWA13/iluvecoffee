a551e1185aef18d282a8fb93faf67344b68b4cbf
Junior Hirwa <iamhirwejr@gmail.com>
2023-12-07 11:37:29 +0200

    feat: add coffee data manipulation methods

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
-export class CoffeesService {}
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+    {
+      id: 2,
+      name: 'Solo Sol',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+    {
+      id: 3,
+      name: 'Colombia Narino',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ]; // let's use this array of coffes as our data base for now
+
+  findAll() {
+    return this.coffees;
+  }
+  findOne(id: string) {
+    return this.coffees.find((item) => item.id === +id);
+  }
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      // update the existing entity
+    }
+  }
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex > 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
