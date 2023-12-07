87802f27f5e5de057ad873ad1f0665545fc5ec68
Junior Hirwa <iamhirwejr@gmail.com>
2023-12-07 11:00:58 +0200

    feat: add coffees service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
 import { BuyersController } from './buyers/buyers.controller';
+import { CoffeesService } from './coffees/coffees.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CoffeesController, BuyersController],
-  providers: [AppService],
+  providers: [AppService, CoffeesService],
 })
 export class AppModule {}
