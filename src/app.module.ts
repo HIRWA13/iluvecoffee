3e723c5abce3e3a76533776113b4e9322acfd57f
Junior Hirwa <iamhirwejr@gmail.com>
2023-12-07 14:48:32 +0200

    feat: add buyers services

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
 import { BuyersController } from './buyers/buyers.controller';
 import { CoffeesService } from './coffees/coffees.service';
+import { BuyersService } from './buyers/buyers.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CoffeesController, BuyersController],
-  providers: [AppService, CoffeesService],
+  providers: [AppService, CoffeesService, BuyersService],
 })
 export class AppModule {}
