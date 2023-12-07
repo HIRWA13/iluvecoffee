981b9b12de401b1775263b948ddfe21165565383
Junior Hirwa <iamhirwejr@gmail.com>
2023-12-07 17:53:39 +0200

    feat: update the app  module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeesController } from './coffees/coffees.controller';
-import { BuyersController } from './buyers/buyers.controller';
-import { CoffeesService } from './coffees/coffees.service';
-import { BuyersService } from './buyers/buyers.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { BuyersModule } from './buyers/buyers.module';
 
 @Module({
-  imports: [CoffeesModule],
-  controllers: [AppController, CoffeesController, BuyersController],
-  providers: [AppService, CoffeesService, BuyersService],
+  imports: [CoffeesModule, BuyersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
