3d88c6c6e540b235f239ea8f3e5f8b45aded1ded
Junior Hirwa <iamhirwejr@gmail.com>
2023-12-08 15:47:02 +0200

    feat: update the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { BuyersModule } from './buyers/buyers.module';
+import { MenuModule } from './menu/menu.module';
 
 @Module({
-  imports: [CoffeesModule, BuyersModule],
+  imports: [CoffeesModule, BuyersModule, MenuModule],
   controllers: [AppController],
   providers: [AppService],
 })
