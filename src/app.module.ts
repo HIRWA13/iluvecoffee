79f1055abfe06e75f25b1656e9fe41bea67072b0
Junior Hirwa <iamhirwejr@gmail.com>
2023-12-09 12:13:17 +0200

    feat: initiate typeorm with a postgress docker container

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { BuyersModule } from './buyers/buyers.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule, BuyersModule],
+  imports: [
+    CoffeesModule,
+    BuyersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
