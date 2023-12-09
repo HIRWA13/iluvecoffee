d4050cdc5b3b090c8babde8d9ef2221a1c841874
Junior Hirwa <iamhirwejr@gmail.com>
2023-12-09 17:26:52 +0200

    feat: add coffees entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Coffee {
+  @PrimaryGeneratedColumn() // automatically generated Id
   id: number;
+
+  @Column() // required by default
   name: string;
+
+  @Column() // required by default
   brand: string;
+
+  @Column('json', { nullable: true }) // here we specify the kind of data to take in and allow the columns to be empty or optional
   flavors: string[];
 }
