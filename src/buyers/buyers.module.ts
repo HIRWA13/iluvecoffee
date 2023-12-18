78cc915b0b8b7a2735bc5f300c4bf67b8bc9cdd9
Junior Hirwa <iamhirwejr@gmail.com>
2023-12-18 11:01:30 +0200

    feat: update buyers module

--- src/buyers/buyers.module.ts
+++ src/buyers/buyers.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BuyersController } from './buyers.controller';
 import { BuyersService } from './buyers.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Buyer } from './entities/buyer.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Buyer])],
+  imports: [],
   controllers: [BuyersController],
   providers: [BuyersService],
 })
