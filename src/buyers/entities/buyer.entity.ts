09552c1126262b3a2dad171dbf5a2b8fec5ed48d
Junior Hirwa <iamhirwejr@gmail.com>
2023-12-18 11:02:19 +0200

    feat: update buyers entity

--- src/buyers/entities/buyer.entity.ts
+++ src/buyers/entities/buyer.entity.ts
@@ -1,16 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
-@Entity()
 export class Buyer {
-  @PrimaryGeneratedColumn()
   id: number;
-
-  @Column()
   name: string;
-
-  @Column()
   location: string;
-
-  @Column()
   isVip: boolean;
 }
